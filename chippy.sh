bbfcddb32d379e0e91e6e60d3ea3e6469fe34a6c
david <david@davids-MacBook-Pro.local>
2017-05-04 17:56:45 -0700

    shell building

--- chippy.sh
+++ chippy.sh
@@ -3,10 +3,4 @@
 echo setting up chippy
 
 ./configure/lib-setup.sh chippy
-
-if [ ! -d "libs/node-lib" ]; then 
-	git clone https://github.com/davidsi/node-lib.git libs/node-lib
-	echo "cd libs/node-lib; git stash save; git pull; git stash pop; cd ../.. ">>git-sync.sh
-else 
-	echo "node library repo already present"
-fi
+./configure/lib-setup.sh libs/node-lib

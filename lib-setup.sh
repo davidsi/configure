7d8db3f5419ec377e817f6bb2c4d28629b07ebf8
david <david@davids-MacBook-Pro.local>
2017-05-05 13:27:16 -0700

    splitt

--- lib-setup.sh
+++ lib-setup.sh
@@ -6,6 +6,13 @@ if [ "$2" == "" ]; then
 	if [ ! -d "$1" ]; then 
 		git clone https://github.com/davidsi/$1.git 
 		echo "cd $1; git stash save; git pull; git stash pop; cd .. ">>git-sync.sh
+		if [ -f "$1/configure.sh" ]; then
+			chmod 777 $1/configure.sh
+			$1/configure.sh
+			cd $1
+			git checkout -- configure.sh
+			cd ..
+		fi
 	else 
 		echo "$1 repo already present"
 	fi
@@ -15,6 +22,13 @@ elif  [ "$1" == "lib" ]; then
 	if [ ! -d "libs/$2" ]; then 
 		git clone https://github.com/davidsi/$2.git libs/$2
 		echo "cd libs/$2; git stash save; git pull; git stash pop; cd ../.. ">>git-sync.sh
+		if [ -f "libs/$2/configure.sh" ]; then
+			chmod 777 libs/$2/configure.sh
+			libs/$2/configure.sh
+			cd libs/$2
+			git checkout -- configure.sh
+			cd ../..
+		fi
 	else 
 		echo "$2 repo already present"
 	fi

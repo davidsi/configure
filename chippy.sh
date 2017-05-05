30a017ed5b4b1c9614ece1980268226a64df5155
david <david@davids-MacBook-Pro.local>
2017-05-04 17:45:51 -0700

    shell building

--- chippy.sh
+++ chippy.sh
@@ -2,14 +2,14 @@
 #
 echo setting up chippy
 
-if [ ! -d chippy ]; then 
+if [ ! -d "chippy" ]; then 
 	git clone https://github.com/davidsi/chippy.git 
 	echo "cd chippy; git stash save; git pull; git stash pop; cd .. ">>git-sync.sh
 else 
 	echo "chippy repo already present"
 fi
 
-if [ ! -d libs/node-lib ]; then 
+if [ ! -d "libs/node-lib" ]; then 
 	git clone https://github.com/davidsi/node-lib.git libs/node-lib
 	echo "cd libs/node-lib; git stash save; git pull; git stash pop; cd ../.. ">>git-sync.sh
 else 

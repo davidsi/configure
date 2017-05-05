bf1efefbdb4d15886ed9a5317c13cfcab59adace
david <david@davids-MacBook-Pro.local>
2017-05-04 17:59:55 -0700

    shell building

--- lib-setup.sh
+++ lib-setup.sh
@@ -1,11 +1,24 @@
 # generic setup
 #
-echo setting up "$1"
+if [ "$2" == "" ]; then 
+	echo setting up "$1"
 
-if [ ! -d "$1" ]; then 
-	git clone https://github.com/davidsi/$1.git 
-	echo "cd $1; git stash save; git pull; git stash pop; cd .. ">>git-sync.sh
-else 
-	echo "$1 repo already present"
+	if [ ! -d "$1" ]; then 
+		git clone https://github.com/davidsi/$1.git 
+		echo "cd $1; git stash save; git pull; git stash pop; cd .. ">>git-sync.sh
+	else 
+		echo "$1 repo already present"
+	fi
+elif  [ "$1" == "lib" ]; then 
+	echo setting up "$2"
+
+	if [ ! -d "libs/$2" ]; then 
+		git clone https://github.com/davidsi/$2.git libs/$2
+		echo "cd libs/$2; git stash save; git pull; git stash pop; cd ../.. ">>git-sync.sh
+	else 
+		echo "$2 repo already present"
+	fi
+else
 fi
+
 

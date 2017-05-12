0bfae5c8501dc7308616524d3d8e79ae7f25c3ec
david <david@davids-MacBook-Pro.local>
2017-05-11 20:59:24 -0700

    shell for all projects

---
+++ all.sh
@@ -0,0 +1,14 @@
+# get everything
+#
+./configure/lib-setup.sh lib arduino-lib
+./configure/lib-setup.sh boat-arduino
+./configure/lib-setup.sh boat-devices
+./configure/lib-setup.sh boat-ios
+./configure/lib-setup.sh chippy
+./configure/lib-setup.sh gmail-example
+./configure/lib-setup.sh hardware
+./configure/lib-setup.sh lib node-lib
+./configure/lib-setup.sh lib rov-arduino
+./configure/lib-setup.sh lib rov-node
+./configure/lib-setup.sh weather-station
+

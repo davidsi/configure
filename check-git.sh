eb58c27d37513af58168a8a7ca4d671c5316f111
david <david@davids-MacBook-Pro.local>
2017-05-12 09:14:57 -0700

    enumerate script

--- check-git.sh
+++ check-git.sh
@@ -1,3 +1,5 @@
+# from http://stackoverflow.com/questions/3258243/check-if-pull-needed-in-git?noredirect=1&lq=1
+#
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
---
+++ enumerate-all-folders.sh
@@ -0,0 +1,10 @@
+# enumerate all the folders
+#
+for f in *; do
+    if [[ -d $f ]]; then
+        cd $f
+        pwd
+        ../configure/check-git.sh
+        cd ..
+    fi
+done
